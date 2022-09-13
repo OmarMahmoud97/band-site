05eb968da0aea7b75f2b9162a34878e74b3617cf
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2022-09-13 21:04:19 +0100

    add js for shows page to display array of tickets in a div for each individual object

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -22,29 +22,27 @@ let madeComments = [
       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
   },
 ];
-console.log(madeComments);
 
 let comments = document.querySelector(".conversation__section");
 let div = document.querySelector(".div");
 
 for (let i = 0; i < madeComments.length; i++) {
-  console.log(i);
-
   let newDiv = document.createElement("div");
-  newDiv.classList.add("comment__item");
+  newDiv.classList.add("conversation__item");
   comments.appendChild(newDiv);
+
   let nameEl = document.createElement("h5");
-  nameEl.classList.add("nameMade");
+  nameEl.classList.add("conversation__name");
   nameEl.innerText = madeComments[i].name;
   newDiv.appendChild(nameEl);
 
   let dateEl = document.createElement("p");
-  dateEl.classList.add("dateMade");
+  dateEl.classList.add("conversation__date");
   dateEl.innerText = madeComments[i].date;
   newDiv.appendChild(dateEl);
 
   let commentEl = document.createElement("p");
-  commentEl.classList.add("commentMade");
+  commentEl.classList.add("conversation__comment");
   commentEl.innerText = madeComments[i].comment;
   newDiv.appendChild(commentEl);
 }
