bc0f7ca69b9ceeca6a662576868241e1259a9e61
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2022-09-13 15:59:25 +0100

    create a function that places the premade comments into the index.html page

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -6,7 +6,6 @@ let madeComments = [
     date: "02/17/2021",
     comment:
       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-    class: "comments-pre--topBorder",
     pictureClass: "comments-pre__circle",
   },
   {
@@ -21,20 +20,31 @@ let madeComments = [
     date: " 12/20/2020",
     comment:
       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-    class: "comments-pre--bottomBorder",
-    pictureClass: "comments-pre__circle",
   },
 ];
 console.log(madeComments);
 
-const comments = document.querySelector(".conversation__section");
-
-let element = document.createElement("p");
-element.innerText = madeComments[0];
-comments.appendChild(element);
+let comments = document.querySelector(".conversation__section");
+let div = document.querySelector(".div");
 
 for (let i = 0; i < madeComments.length; i++) {
-  element.innerText = madeComments[i];
-  comments.appendChild(element);
-  console.log(madeComments[i]);
+  console.log(i);
+
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("comment__item");
+  comments.appendChild(newDiv);
+  let nameEl = document.createElement("h5");
+  nameEl.classList.add("nameMade");
+  nameEl.innerText = madeComments[i].name;
+  newDiv.appendChild(nameEl);
+
+  let dateEl = document.createElement("p");
+  dateEl.classList.add("dateMade");
+  dateEl.innerText = madeComments[i].date;
+  newDiv.appendChild(dateEl);
+
+  let commentEl = document.createElement("p");
+  commentEl.classList.add("commentMade");
+  commentEl.innerText = madeComments[i].comment;
+  newDiv.appendChild(commentEl);
 }
