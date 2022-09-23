7c2ead9f7e57a1d1aaff78d60343836fb7b32619
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2022-09-23 10:38:34 +0100

    add api call for show times

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,35 +1,14 @@
-let showsObject = [
-  {
-    date: "Mon Sept 06 2021",
-    venue: "Ronald Lane",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Tue Sept 21 2021",
-    venue: "Pier 3 East",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Fri Oct 15 2021 ",
-    venue: "View Lounge ",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Sat Nov 06 2021",
-    venue: "Hyatt Agency",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Fri Nov 26 2021",
-    venue: "Moscow Center",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Wed Dec 15 2021",
-    venue: "Press Club",
-    location: "San Francisco, CA",
-  },
-];
+let showsObject = [];
+
+axios
+  .get(
+    "https://project-1-api.herokuapp.com/showdates?api_key=d7bef51b-651b-4a63-86e1-2327e85bd335"
+  )
+  .then((response) => {
+    console.log(response.data);
+    showsObject = response.data;
+    buildTickets();
+  });
 
 let ticketSection = document.querySelector(".tickets");
 
@@ -81,76 +60,80 @@ const buildDesktopLabels = () => {
 
 buildDesktopLabels();
 
-for (let i = 0; i < showsObject.length; i++) {
-  let newDiv = document.createElement("div");
-  newDiv.classList.add("tickets__item");
-  ticketBooking.appendChild(newDiv);
+const buildTickets = () => {
+  const showsItem = document.querySelector(".tickets");
 
-  let dateHeader = document.createElement("h4");
-  dateHeader.classList.add("tickets__date-header");
-  dateHeader.innerText = "Date";
-  newDiv.appendChild(dateHeader);
+  for (let i = 0; i < showsObject.length; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("tickets__item");
+    showsItem.appendChild(newDiv);
 
-  let ticketDate = document.createElement("p");
-  ticketDate.classList.add("tickets__date");
-  ticketDate.innerText = showsObject[i].date;
-  newDiv.appendChild(ticketDate);
+    let dateHeader = document.createElement("h4");
+    dateHeader.classList.add("tickets__date-header");
+    dateHeader.innerText = "Date";
+    newDiv.appendChild(dateHeader);
 
-  let dateWrapper = document.createElement("div");
-  dateWrapper.classList.add("tickets__date-wrapper");
-  newDiv.appendChild(dateWrapper);
+    let ticketDate = document.createElement("p");
+    ticketDate.classList.add("tickets__date");
+    ticketDate.innerText = showsObject[i].date;
+    newDiv.appendChild(ticketDate);
 
-  let ticketDateTablet = document.createElement("p");
-  ticketDateTablet.classList.add("tickets__date-tablet");
-  ticketDateTablet.innerText = showsObject[i].date;
-  dateWrapper.appendChild(ticketDateTablet);
+    let dateWrapper = document.createElement("div");
+    dateWrapper.classList.add("tickets__date-wrapper");
+    newDiv.appendChild(dateWrapper);
 
-  let venueHeader = document.createElement("h4");
-  venueHeader.classList.add("tickets__venue-header");
-  venueHeader.innerText = "Venue";
-  newDiv.appendChild(venueHeader);
+    let ticketDateTablet = document.createElement("p");
+    ticketDateTablet.classList.add("tickets__date-tablet");
+    ticketDateTablet.innerText = showsObject[i].date;
+    dateWrapper.appendChild(ticketDateTablet);
 
-  let ticketVenue = document.createElement("p");
-  ticketVenue.classList.add("tickets__venue");
-  ticketVenue.innerText = showsObject[i].venue;
-  newDiv.appendChild(ticketVenue);
+    let venueHeader = document.createElement("h4");
+    venueHeader.classList.add("tickets__venue-header");
+    venueHeader.innerText = "Venue";
+    newDiv.appendChild(venueHeader);
 
-  let venueWrapper = document.createElement("div");
-  venueWrapper.classList.add("tickets__venue-wrapper");
-  newDiv.appendChild(venueWrapper);
+    let ticketVenue = document.createElement("p");
+    ticketVenue.classList.add("tickets__venue");
+    ticketVenue.innerText = showsObject[i].place;
+    newDiv.appendChild(ticketVenue);
 
-  let venueTablet = document.createElement("p");
-  venueTablet.classList.add("tickets__venue-tablet");
-  venueTablet.innerText = showsObject[i].venue;
-  venueWrapper.appendChild(venueTablet);
+    let venueWrapper = document.createElement("div");
+    venueWrapper.classList.add("tickets__venue-wrapper");
+    newDiv.appendChild(venueWrapper);
 
-  let locationHeader = document.createElement("h4");
-  locationHeader.classList.add("tickets__location-header");
-  locationHeader.innerText = "Location";
-  newDiv.appendChild(locationHeader);
+    let venueTablet = document.createElement("p");
+    venueTablet.classList.add("tickets__venue-tablet");
+    venueTablet.innerText = showsObject[i].place;
+    venueWrapper.appendChild(venueTablet);
 
-  let ticketLocation = document.createElement("p");
-  ticketLocation.classList.add("tickets__location");
-  ticketLocation.innerText = showsObject[i].location;
-  newDiv.appendChild(ticketLocation);
+    let locationHeader = document.createElement("h4");
+    locationHeader.classList.add("tickets__location-header");
+    locationHeader.innerText = "Location";
+    newDiv.appendChild(locationHeader);
 
-  let locationWrapper = document.createElement("div");
-  locationWrapper.classList.add("tickets__location-wrapper");
-  newDiv.appendChild(locationWrapper);
+    let ticketLocation = document.createElement("p");
+    ticketLocation.classList.add("tickets__location");
+    ticketLocation.innerText = showsObject[i].location;
+    newDiv.appendChild(ticketLocation);
 
-  let locationTablet = document.createElement("p");
-  locationTablet.classList.add("tickets__venue-tablet");
-  locationTablet.innerText = showsObject[i].location;
-  locationWrapper.appendChild(locationTablet);
+    let locationWrapper = document.createElement("div");
+    locationWrapper.classList.add("tickets__location-wrapper");
+    newDiv.appendChild(locationWrapper);
 
-  let btnWrapper = document.createElement("div");
-  btnWrapper.classList.add("tickets__btn-wrapper");
-  newDiv.appendChild(btnWrapper);
+    let locationTablet = document.createElement("p");
+    locationTablet.classList.add("tickets__venue-tablet");
+    locationTablet.innerText = showsObject[i].location;
+    locationWrapper.appendChild(locationTablet);
 
-  let ticketBtn = document.createElement("a");
-  ticketBtn.classList.add("tickets__btn");
-  ticketBtn.setAttribute("href", "https://youtu.be/dQw4w9WgXcQ");
-  ticketBtn.target = "_blank";
-  ticketBtn.innerText = "BUY TICKETS";
-  btnWrapper.appendChild(ticketBtn);
-}
+    let btnWrapper = document.createElement("div");
+    btnWrapper.classList.add("tickets__btn-wrapper");
+    newDiv.appendChild(btnWrapper);
+
+    let ticketBtn = document.createElement("a");
+    ticketBtn.classList.add("tickets__btn");
+    ticketBtn.setAttribute("href", "https://youtu.be/dQw4w9WgXcQ");
+    ticketBtn.target = "_blank";
+    ticketBtn.innerText = "BUY TICKETS";
+    btnWrapper.appendChild(ticketBtn);
+  }
+};
